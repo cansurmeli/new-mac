99addb78eeb0b6344321d41d34434e2fb1121f4c
Can Sürmeli <c_surmeli@icloud.com>
2018-10-16 15:23:42 +0200

    Updated `caskfile.sh`.

--- caskfile.sh
+++ caskfile.sh
@@ -1,28 +1,42 @@
 # Design
+brew cask install abstract
+brew cask install reduce
 brew cask install sketch
 brew cask install zeplin
 
 # Development
-brew cask install iterm2
 brew cask install docker
+brew cask install firefox
 brew cask install mamp
 brew cask install postman
 brew cask install qgis
+brew cask install sequel-pro
 brew cask install vmware-fusion
 
 # Media
 brew cask install vlc
 
-# Productivity
-brew cask install alfred
-brew cask install appcleaner
-brew cask install istat-menus
-brew cask install kap
-brew cask install nordvpn
-
 # QuickLook plugins
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo
+brew cask install qlcolorcode
+brew cask install qlstephen
+brew cask install qlmarkdown
+brew cask install quicklook-json
+brew cask install qlprettypatch
+brew cask install quicklook-csv
+brew cask install betterzipql
+brew cask install qlimagesize
+brew cask install webpquicklook
+brew cask install suspicious-package
+brew cask install quicklookase
+brew cask install qlvideo
 
 # Tools
+brew cask install appcleaner
+brew cask install backblaze
+brew cask install colloquy
+brew cask install istat-menus
+brew cask install kap
+brew cask install skype
+brew cask install tor-browser
 brew cask install transmission
-
+brew cask install tunnelblick
