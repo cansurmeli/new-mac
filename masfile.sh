0672673d9d9e09709bee3070cb65f47b66cec32b
Can Sürmeli <c_surmeli@icloud.com>
2018-10-16 15:24:11 +0200

    Updated `masfile.sh`.

--- masfile.sh
+++ masfile.sh
@@ -1,10 +1,16 @@
-mas install 443987910 # 1Password
-mas install 411643860 # DaisyDisk
-mas install 775737590 # iA Writer
-mas install 731738567 # Permute
-mas install 407963104 # Pixelmator
-mas install 413965349 # Soulver
-mas install 904280696 # Things
-mas install 557168941 # Tweetbot
-mas install 497799835 # Xcode
-mas install 889428659 # xScope
+mas install 443987910			# 1Password
+mas install 931657367			# Calcbot
+mas install 411643860			# DaisyDisk
+mas install 775737590			# iA Writer
+mas install 405772121			# LittleIpsum
+mas install 731738567			# Permute
+mas install 407963104			# Pixelmator
+mas install 1289583905		# Pixelmator Pro
+mas install 413965349			# Soulver
+mas install 747648890			# Telegram
+mas install 425424353			# The Unarchiver
+mas install 904280696			# Things
+mas install 557168941			# Tweetbot
+mas install 1284863847		# Unsplash Wallpapers
+mas install 497799835			# Xcode
+mas install 889428659			# xScope
