a8107401b4c4e32992f36a1d6937601df2566525
Can Sürmeli <c_surmeli@icloud.com>
2018-10-16 15:05:43 +0200

    Extended/commented `brewfile.sh`.

--- brewfile.sh
+++ brewfile.sh
@@ -1,5 +1,13 @@
-brew install wget
-brew install youtube-dl
-brew install mas
-brew install imagemagick
-
+brew install bat								# Clone of cat(1) with syntax highlighting and Git integration
+brew install colordiff					# Color-highlighted diff(1) output 
+brew install ffmpeg							# Play, record, convert, and stream audio and video
+brew install highlight					# Convert source code to formatted text with syntax highlighting
+brew install hug								# Configurable static site generatoro
+brew install imagemagick				# Tools and libraries to manipulate images in many formats
+brew install mas								# Mac App Store command-line interface
+brew install pandoc							# Swiss-army knife of markup format conversion
+brew install python							# Interpreted, interactive, object-oriented programming language
+brew install rsync							# Utility that provides fast incremental file transfer
+brew install speedtest-cli			# Command-line interface for https://speedtest.net bandwidth tests
+brew install wget								# Internet file retriever
+brew install youtube-dl					# Download YouTube videos from the command-line
