2538aeae77c7fec0c52cd193e1b3c25529084e7e
Can Sürmeli <c_surmeli@icloud.com>
2019-10-08 11:16:31 +0200

    Minor modifications to adjust with my current taste.

--- brewfile.sh
+++ brewfile.sh
@@ -1,19 +1,22 @@
 brew install bat								# Clone of cat(1) with syntax highlighting and Git integration
-brew install carthage						# Decentralized dependency manager for Cocoa
 brew install colordiff					# Color-highlighted diff(1) output 
 brew install ffmpeg							# Play, record, convert, and stream audio and video
 brew install highlight					# Convert source code to formatted text with syntax highlighting
-brew install ghc								# Glorious Glasgow Haskell Compilation System
 brew install highlight					# Convert source code to formatted text with syntax highlighting
 brew install hugo								# Configurable static site generatoro
 brew install imagemagick				# Tools and libraries to manipulate images in many formats
 brew install mas								# Mac App Store command-line interface
-brew install octave							# High-level interpreted language for numerical computing
 brew install pandoc							# Swiss-army knife of markup format conversion
-brew install python							# Interpreted, interactive, object-oriented programming language
 brew install rename							# Perl-powered file rename script with many helpful built-ins
 brew install restic							# Fast, efficient and secure backup program
 brew install rsync							# Utility that provides fast incremental file transfer
 brew install speedtest-cli			# Command-line interface for https://speedtest.net bandwidth tests
+brew install vim								# Vi IMproved, a programmer's text editor
 brew install wget								# Internet file retriever
 brew install youtube-dl					# Download YouTube videos from the command-line
+
+# POSSIBLE ADDITIONS
+#brew install carthage						# Decentralized dependency manager for Cocoa
+#brew install ghc								# Glorious Glasgow Haskell Compilation System
+#brew install octave							# High-level interpreted language for numerical computing
+#brew install python							# Interpreted, interactive, object-oriented programming language
--- caskfile.sh
+++ caskfile.sh
@@ -1,8 +1,8 @@
 # DESIGN
-brew cask install abstract
-brew cask install reduce
+#brew cask install abstract
+#brew cask install reduce
 brew cask install sketch
-brew cask install zeplin
+#brew cask install zeplin
 
 # DEVELOPMENT
 brew cask install docker
@@ -13,23 +13,26 @@ brew cask install mamp
 brew cask install postman
 brew cask install qgis
 brew cask install sequel-pro
+brew cask install virtualbox
 
 # MEDIA
 brew cask install iina
+brew cask install plex
 
 # QUICKlOOK PLUGINS
-brew cask install qlcolorcode
-brew cask install qlstephen
-brew cask install qlmarkdown
-brew cask install quicklook-json
-brew cask install qlprettypatch
-brew cask install quicklook-csv
-brew cask install betterzipql
-brew cask install qlimagesize
-brew cask install webpquicklook
-brew cask install suspicious-package
-brew cask install quicklookase
-brew cask install qlvideo
+# Catalina breaks these because they're not notarised.
+#brew cask install qlcolorcode
+#brew cask install qlstephen
+#brew cask install qlmarkdown
+#brew cask install quicklook-json
+#brew cask install qlprettypatch
+#brew cask install quicklook-csv
+#brew cask install betterzipql
+#brew cask install qlimagesize
+#brew cask install webpquicklook
+#brew cask install suspicious-package
+#brew cask install quicklookase
+#brew cask install qlvideo
 
 # TOOLS
 brew cask install appcleaner
@@ -37,14 +40,12 @@ brew cask install colloquy
 brew cask install istat-menus
 brew cask install istat-server
 brew cask install kap
-brew cask install mactex
+#brew cask install mactex
 brew cask install mathpix-snipping-tool
-brew cask install owncloud
 brew cask install pdfinfo
 brew cask install skype
-brew cask install tor-browser
 brew cask install transmission
-brew cask install tunnelblick
+#brew cask install tunnelblick
 brew cask install suspicious-package		# All cool, actually a security inspection tool.
 
 # OTHER
