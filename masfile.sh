233b57ca11b64a2f886058e8c1d4686db4df5243
Can Sürmeli <cansurmeli@outlook.com>
2021-10-27 16:51:50 +0000

    Updated masfile.

--- masfile.sh
+++ masfile.sh
@@ -1,7 +1,7 @@
 mas install 931657367			# Calcbot
 mas install 775737590			# iA Writer
 mas install 405772121			# LittleIpsum
-mas install 1289583905		# Pixelmator Pro
-mas install 904280696			# Things
-mas install 1284863847		# Unsplash Wallpapers
+mas install 1289583905		    # Pixelmator Pro
+#mas install 904280696			# Things
+mas install 1284863847		    # Unsplash Wallpapers
 mas install 497799835			# Xcode
